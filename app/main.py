c743a07524e26ccfe46cc257a6d9e149bdf384cb
Andre Marcio <andre.marcio.ti@gmail.com>
2026-05-02 12:03:08 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.strip().split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, src_file, dst_file = parts
+
+    if src_file == dst_file:
+        return
+
+    try:
+        with open(src_file, "rb") as file_in, open(dst_file, "wb") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
